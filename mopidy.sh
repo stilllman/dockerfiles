0c482f9b41ab7894c72b6cdde2ea0bfa7db0ca82
Luc Touraille <luc_touraille@yahoo.fr>
2016-06-08 22:22:17 +0100

    Correct replacement in config file.

--- mopidy.sh
+++ mopidy.sh
@@ -3,7 +3,7 @@
 if [ -n "$SPOTIFY_USERNAME" ] && [ -n "$SPOTIFY_PASSWORD" ]; then
     sed -i.bak "s/\[spotify\]/[spotify]\nusername=${SPOTIFY_USERNAME}\npassword=${SPOTIFY_PASSWORD}/" /root/.config/mopidy/mopidy.conf
 else
-    sed -i.bak 's/[spotify]/[spotify]\nenabled=false/' /root/.config/mopidy/mopidy.conf
+    sed -i.bak 's/\[spotify\]/[spotify]\nenabled=false/' /root/.config/mopidy/mopidy.conf
 fi
 
 exec mopidy $@
