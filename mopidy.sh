46465269d36226643617b76090389f1cc961a1e9
Luc Touraille <luc_touraille@yahoo.fr>
2016-06-26 21:33:16 +0100

    Add logging and remove backup files when configuring settings

--- mopidy.sh
+++ mopidy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 if [ -n "$SPOTIFY_USERNAME" ] && [ -n "$SPOTIFY_PASSWORD" ]; then
-    sed -i.bak "s/\[spotify\]/[spotify]\nusername=${SPOTIFY_USERNAME}\npassword=${SPOTIFY_PASSWORD}/" /root/.config/mopidy/mopidy.conf
+    echo "Enabling Spotify..."
+    sed -i "s/\[spotify\]/[spotify]\nusername=${SPOTIFY_USERNAME}\npassword=${SPOTIFY_PASSWORD}/" /root/.config/mopidy/mopidy.conf
 else
-    sed -i.bak 's/\[spotify\]/[spotify]\nenabled=false/' /root/.config/mopidy/mopidy.conf
+    echo "Disabling Spotify..."
+    sed -i 's/\[spotify\]/[spotify]\nenabled=false/' /root/.config/mopidy/mopidy.conf
 fi
 
 if [ -n "$LASTFM_USERNAME" ] && [ -n "$LASTFM_PASSWORD" ]; then
-    sed -i.bak "s/\[scrobbler\]/[scrobbler]\nusername=${LASTFM_USERNAME}\npassword=${LASTFM_PASSWORD}/" /root/.config/mopidy/mopidy.conf
+    echo "Enabling LastFM scrobbling..."
+    sed -i "s/\[scrobbler\]/[scrobbler]\nusername=${LASTFM_USERNAME}\npassword=${LASTFM_PASSWORD}/" /root/.config/mopidy/mopidy.conf
 else
-    sed -i.bak 's/\[scrobbler\]/[scrobbler]\nenabled=false/' /root/.config/mopidy/mopidy.conf
+    echo "Disabling LastFM scrobbling..."
+    sed -i 's/\[scrobbler\]/[scrobbler]\nenabled=false/' /root/.config/mopidy/mopidy.conf
 fi
 
 exec mopidy $@
